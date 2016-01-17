0df9a6e01fc45172de8b4535ec1ee681eff7e271
Ionică Bizău <bizauionica@gmail.com>
2016-01-17 12:22:53 +0200

    Fixing the tests

--- test/index.js
+++ test/index.js
@@ -47,47 +47,82 @@ tester.describe("working with objects in memeory", test => {
         });
     });
 });
-tester.describe("working with objects in memeory", test => {
-    var app = new EngineApp(`${__dirname}/app`)
-      , em = new EntrypointManager(app)
-      ;
 
-    test.it("sets entrypoints", () => {
-        em.set("example.com", "private", true);
-        test.expect(em.get()).toEqual({
-            private: {
-                "example.com": "private"
-            }
-          , public: {}
-        });
-        eem.set("example.com", "public_layout", false);
-        test.expect(eem.get()).toEqual({
-            private: {
-                "example.com": "private"
-            }
-          , public: {
-                "example.com": "public_layout"
-            }
+tester.describe("working with package.json files", test => {
+    var em = new EntrypointManager(new EngineApp(`${__dirname}/app`));
+    test.it("sets entrypoints", (cb) => {
+        em.set("example.com", "private", true, function (err) {
+            test.expect(err).toBe(null);
+            em.get(function (err, entrypoints) {
+                test.expect(err).toBe(null);
+                test.expect(entrypoints).toEqual({
+                    private: {
+                        "example.com": "private"
+                    }
+                  , public: {}
+                });
+                cb();
+            })
         });
     });
-    test.it("deletes an etntrypoint", () => {
-        eem.delete("example.com", true);
-        test.expect(eem.get()).toEqual({
-            private: {
-                "example.com": undefined
-            }
-          , public: {
-                "example.com": "public_layout"
-            }
+
+    test.it("set public entrypoints", (cb) => {
+        em.set("example.com", "public_layout", false, function (err) {
+            test.expect(err).toBe(null);
+            em.get(function (err, entrypoints) {
+                test.expect(err).toBe(null);
+                test.expect(entrypoints).toEqual({
+                    private: {
+                        "example.com": "private"
+                    }
+                  , public: {
+                        "example.com": "public_layout"
+                    }
+                });
+                cb();
+            })
         });
-        eem.delete("example.com", false);
-        test.expect(eem.get()).toEqual({
-            private: {
-                "example.com": undefined
-            }
-          , public: {
-                "example.com": undefined
-            }
+    });
+
+    test.it("deletes a private etntrypoint", () => {
+        eem.delete("example.com", true, function (err) {
+            em.get(function (err, entrypoints) {
+                test.expect(err).toBe(null);
+                test.expect(entrypoints).toEqual({
+                    private: {}
+                  , public: {
+                        "example.com": "public_layout"
+                    }
+                });
+                cb();
+            })
+        });
+    });
+
+    test.it("deletes a public etntrypoint", () => {
+        eem.delete("example.com", false, function (err) {
+            eem.get(function (err, entrypoints) {
+                test.expect(entrypoints).toEqual({
+                    private: {
+                        "example.com": undefined
+                    }
+                  , public: {
+                        "example.com": undefined
+                    }
+                });
+            });
+        });
+    });
+
+    test.it("deletes a private etntrypoint", () => {
+        eem.delete("example.com", true, function (err) {
+            eem.get(function (err, entrypoints) {
+                test.expect(entrypoints).toEqual({
+                    private: {}
+                  , public: {}
+                });
+                cb();
+            });
         });
     });
 });
